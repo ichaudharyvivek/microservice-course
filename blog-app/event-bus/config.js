390ef4cc7e257dfcb2f09756afcdb63e30347f08
Vivek Chaudhary <vkc369258@gmail.com>
2024-03-20 18:07:42 +0000

    Update config.js

--- blog-app/event-bus/config.js
+++ blog-app/event-bus/config.js
@@ -3,7 +3,8 @@ const config = {
     POST_SERVICE: '4000-ichaudharyv-microservic-5gdh78ogrj9.ws-us110.gitpod.io',
     COMMENT_SERVICE:
       '4001-ichaudharyv-microservic-5gdh78ogrj9.ws-us110.gitpod.io',
-    QUERY_SERVICE: 'localhost:4003',
+    QUERY_SERVICE:
+      '4002-ichaudharyv-microservic-5gdh78ogrj9.ws-us110.gitpod.io',
   },
 };
 
